b08a036187b092e4fe96409d9304a16525580fcc
George991808 <gellis7@gmail.com>
2021-12-30 20:22:13 +0800

    added random function, removed the double call of confirm and prompt functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,29 @@ var password = document.querySelector(".password");
 
 var characters="";
 var size;
-
+var IntermediatePassword
 function generatePassword(event) {
   event.stopPropagation();
+  characters=""
     askSize();
     askQuestion("Password contains upper case?","QWERTYUIOPASDFGHJKLZXCVBNM");
     askQuestion("Password contains lower case?","qwertyuiopasdfghjklzxcvbnm");
     askQuestion("Password contains special characters?","!@#$%^&*()_+?><,./{}`~");
     askQuestion("Password contains numbers?","1234567890");
-    password.textContent=characters;
+    if ( characters.length>1 ) {
+
+      for (var i = 0; i < size; i++) {
+        
+        IntermediatePassword+=characters.charAt(Math.floor(Math.random()*characters.length))
+        password.textContent=IntermediatePassword
+      }
+    }
+    
+    
   }
 
   function askQuestion(choiceText,lettersToAdd) {
-    confirm(choiceText);
+   
     if (confirm(choiceText)==true) {
       characters=characters+lettersToAdd;
     }
@@ -23,8 +33,8 @@ function generatePassword(event) {
   }
   function askSize() {
     let sizeAnswer = prompt("Enter size between 8 and 128" ,8);
-    if (prompt(sizeAnswer)==true) {
-      size=sizeAnswer
+    if (sizeAnswer !=null) {
+      size=Math.min(Math.max(sizeAnswer,8),128)
     }
     
   }
