521fa4e8bbc5598ffb85a2d7f4f5b3e2ac0f67b3
George991808 <gellis7@gmail.com>
2021-12-30 21:13:36 +0800

    added some initial styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,22 @@ var password = document.querySelector(".password");
 var characters="";
 var size;
 var IntermediatePassword ="";
+// This is called from from the generate button event
+//
 function generatePassword(event) {
   event.stopPropagation();
   characters=""
     askSize();
+    //The Ask question function adds characters to the final selection string, characters
     askQuestion("Password contains upper case?","QWERTYUIOPASDFGHJKLZXCVBNM");
     askQuestion("Password contains lower case?","qwertyuiopasdfghjklzxcvbnm");
     askQuestion("Password contains special characters?","!@#$%^&*()_+?><,./{}`~");
     askQuestion("Password contains numbers?","1234567890");
+   // Has at least one option been set to OK? only generate a password if at least 1 has been selected
     if ( characters.length>1 ) {
 
+      //Pick a random character from the string variables
+      // repeat until desired password length achieved
       for (var i = 0; i < size; i++) {
         
         IntermediatePassword+=characters.charAt(Math.floor(Math.random()*characters.length))
@@ -34,6 +40,7 @@ function generatePassword(event) {
   function askSize() {
     let sizeAnswer = prompt("Enter size between 8 and 128" ,8);
     if (sizeAnswer !=null) {
+      //force an answer between 8 and 128
       size=Math.min(Math.max(sizeAnswer,8),128)
     }
     
