1b023436ea73dac50dab66c819f6801c6c5407da
George991808 <gellis7@gmail.com>
2021-12-30 19:57:22 +0800

    added selections, still getting asked twice for each one not sure why yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,33 @@
-var button = document.querySelector(".button");
+var generatebutton = document.querySelector(".generatebutton");
 var password = document.querySelector(".password");
 
+var characters="";
+var size;
+
 function generatePassword(event) {
-    // event.stopPropagation();
-    password.textContent="hello";
+  event.stopPropagation();
+    askSize();
+    askQuestion("Password contains upper case?","QWERTYUIOPASDFGHJKLZXCVBNM");
+    askQuestion("Password contains lower case?","qwertyuiopasdfghjklzxcvbnm");
+    askQuestion("Password contains special characters?","!@#$%^&*()_+?><,./{}`~");
+    askQuestion("Password contains numbers?","1234567890");
+    password.textContent=characters;
+  }
+
+  function askQuestion(choiceText,lettersToAdd) {
+    confirm(choiceText);
+    if (confirm(choiceText)==true) {
+      characters=characters+lettersToAdd;
+    }
+    
+  }
+  function askSize() {
+    let sizeAnswer = prompt("Enter size between 8 and 128" ,8);
+    if (prompt(sizeAnswer)==true) {
+      size=sizeAnswer
+    }
     
   }
 
 
-button.addEventListener("click", generatePassword);
+generatebutton.addEventListener("click", generatePassword);
